5e28fe598be26c260b544b6534bdfde7eaf79198
Gabriel Souza <gabrielcs922@icloud.com>
2024-03-03 15:18:58 -0500

    Responsivel, logica quando links sobrepoem e semantica

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var icon_share = document.querySelector(".share")
 var icons = document.querySelector(".icones")
 
+
 icon_share.addEventListener("click", (evento) => {
     icons.classList.toggle("aparece")
+})
+
+var icon_desaparece = document.querySelector("#sobreposto")
+icon_desaparece.addEventListener("click", (desaparece) => {
+    icons.classList.add("aparece")
 })
